1c499586f220539d7644680771b70a198697bd84
lapshuntanya <lapshun.tatiana@gmail.com>
2024-09-12 16:50:05 +0300

    isLeapYear()

--- P34-T25-OverloadingOperators/Date.cpp
+++ P34-T25-OverloadingOperators/Date.cpp
@@ -6,17 +6,23 @@ Date::Date()
     time_t now = time(0);
     tm* info = new tm;
     localtime_s(info, &now);
-
-    info->
+        
+    year = info->tm_year + 1900;
+    month = info->tm_mon + 1;
+    day = info->tm_mday;
+    delete info;
 }
 
 Date::Date(int year, int month, int day)
 {
+    this->year = year;
+    this->month = month;
+    this->day = day;
 }
 
 bool Date::isLeapYear() const
 {
-    return false;
+    return year%4 == 0&& year%100!=0 || year%400 == 0 ;
 }
 
 int Date::monthDays() const
