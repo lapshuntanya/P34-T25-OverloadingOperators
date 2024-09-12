e7e2b95f06300fecb43787f9fbc749ce9b68e9ac
lapshuntanya <lapshun.tatiana@gmail.com>
2024-09-12 17:00:15 +0300

    set + get

--- P34-T25-OverloadingOperators/Date.cpp
+++ P34-T25-OverloadingOperators/Date.cpp
@@ -68,32 +68,33 @@ void Date::prevDate()
 
 bool Date::valid() const
 {
-    return false;
+    return (month >= 1 && month <= 12) && (day >= 1 && day <= monthDays());
 }
 
-void Date::setYear(int year)
-{
+void Date::setYear(int year){
+    this->year = year;
 }
 
-int Date::getYear() const
-{
-    return 0;
+int Date::getYear() const{
+    return year;
 }
 
-void Date::setMonth(int month)
-{
+void Date::setMonth(int month){
+    if (month >= 1 && month <= 12)
+        this->month = month;
 }
 
-int Date::getMonth() const
-{
-    return 0;
+int Date::getMonth() const{
+    return month;
 }
 
 void Date::setDay(int day)
 {
+    if (day >= 1 && day <= monthDays())
+        this->day = day;
 }
 
 int Date::getDay() const
 {
-    return 0;
+    return day;
 }
